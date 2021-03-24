bc913461c11a5901ab111373be6d3d2761e241ef
Christophe Le Hy <christophe.lehy@hpe.com>
2021-03-24 13:07:06 +0000

    moved the iscsiX_portgroup variables where they belong and change the name of the user cluster when deploying the VMWare CSI driver

--- scripts/kasten.sh
+++ scripts/kasten.sh
@@ -5,16 +5,21 @@
 . ~/proxy.rc
 cluster=$(kubectl config current-context)
 domain=k8s.org
-version="--version=2.5.25"
-#version=""
+
+if [[ "$1" == "latest" ]]
+then
+  version=""
+else
+  version="--version=2.5.25"
+fi
 
 #
 # Check Syntax
 #
-if (( $# != 0 ))
+if (( $# > 1 ))
 then
-  echo "usage: $0"
-  echo '  deploy Kasten'
+  echo "usage: $0 [latest]"
+  echo "  deploy Kasten version 2.5.25 unless latest is specified"
   exit
 fi
 
@@ -30,10 +35,22 @@ iamthere=$(helm ls -n k10  -f '^k10$' -q)
 if [[ "$iamthere" != "k10" ]]
 then
   helm repo add kasten https://charts.kasten.io/ >/dev/null 2>&1
-  helm install k10 kasten/k10 -n kasten-io ${version}
+  echo Deploying Kasten, this will take a few minutes before all services are up and running
+  helm install k10 kasten/k10 -n kasten-io --wait ${version}
 else
   echo helm chart k10 already installed
 fi
+
+#
+# Wait for all services to become available, the --wait switch in the helm install command above should be enough
+#   status of services can also be queried here: https://kasten.${cluster}.${domain}/k10/dashboardbff-svc/v0/k10HealthCheck
+#
+
+#
+# deploy the S3 location which was created by the user.yml playbook
+#
+echo Deploying the S3 location
+kubectl apply -f ~/.svtrancher/kasten-location.${cluster}.yml
 
 #
 # Deploy the Ingress
@@ -59,3 +76,4 @@ EOF
 kubectl apply -f ${manifest}
 unlink ${manifest}
 kubectl get ingress kasten -n kasten-io
+
