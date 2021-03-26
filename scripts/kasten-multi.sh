c746c8ef271b16ff04c6db60457f4299dffa5f32
Christophe Le Hy <christophe.lehy@hpe.com>
2021-03-26 08:53:23 +0000

    Add script for Kasten multi-cloud management

--- scripts/kasten-multi.sh
+++ scripts/kasten-multi.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 #
 # Here is how one can configure the K10 multi-Cluster manager
-#   in this example, the Kasten running on the cluster cns is the primary instance
-#                    the Kasten running on the cluster clh is the secondary instance
 #
-cat <<EOF
+cat <<EOF >/dev/null
 cd /tmp
 wget https://github.com/kastenhq/external-tools/releases/download/3.0.10/k10multicluster_3.0.10_linux_amd64
 sudo mv ./k10multicluster_3.0.10_linux_amd64 /usr/local/sbin/k10multicluster
 sudo chmod +x /usr/local/sbin/k10multicluster
-
 KUBECONFIG=~/.svtrancher/kube_config.cns \
      k10multicluster setup-primary --name cns
-
-KUBECONFIG=~/.svtrancher/kube_config.cns \
-     k10multicluster bootstrap \
-     --primary-name=cns \
-     --secondary-kubeconfig=/home/chris/.svtrancher/kube_config.clh \
-     --secondary-name=clh \
-     --secondary-cluster-ingress=https://kasten.clh.k8s.org/k10 \
-     --secondary-cluster-ingress-tls-insecure=true \
-
-KUBECONFIG=~/.svtrancher/kube_config.cns \
-     k10multicluster remove \
-     --primary-name=cns \
-     --secondary-kubeconfig=/home/chris/.svtrancher/kube_config.clh \
-     --secondary-name=clh \
-
 EOF
+
+if [[ $# > 1 ]]
+then
+  echo usage: $(basename $0) [remove]
+  echo ' Join (or remove) Kasten on this cluster to (from) the primary Kasten instance running on the CNS clusterr'
+  exit 0
+fi
+
+domain=k8s.org
+cluster=$(kubectl config current-context)
+if [[ $? != 0 ]]
+then
+  echo Please configure KUBECONFIG
+  exit 1
+fi
+
+if [[ $1 == "remove" ]]
+then
+  k10multicluster remove \
+    --primary-kubeconfig=/home/chris/.svtrancher/kube_config.cns \
+    --primary-name=cns \
+    --secondary-name=${cluster}
+else
+  k10multicluster bootstrap \
+    --primary-kubeconfig=/home/chris/.svtrancher/kube_config.cns \
+    --primary-name=cns \
+    --secondary-name=${cluster} \
+    --secondary-cluster-ingress=https://kasten.${cluster}.${domain}/k10 \
+    --secondary-cluster-ingress-tls-insecure=true 
+fi
 
